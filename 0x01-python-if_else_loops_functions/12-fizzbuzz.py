4374ce6f16ec0cc2d62b1c3b6dd6a18e4834e882
Msfead <msfead2015@gmail.com>
2023-11-28 17:10:14 -0800

    M

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i n range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end" ")
+        else print("{:d}".format(i), end=" ")
